846fc0dca8a75d241af06dc16a4ae801c5e3c650
Mohsen Abdollahi <mohsen9april@gmail.com>
2020-05-23 21:47:40 +0430

    Initial Commit

---
+++ MyCoffee/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyCoffee
+//
+//  Created by Mohsen Abdollahi on 5/23/20.
+//  Copyright © 2020 Mohsen Abdollahi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
